b6214bf8eeecf2282d1cdaca6485b45bebb76660
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-02-07 17:14:06 +0100

    fix: first working version

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use axum::{routing::post, Json, Router};
-use kube::core::conversion::{ConversionRequest, ConversionResponse};
+use kube::core::conversion::{ConversionRequest, ConversionResponse, ConversionReview};
 use stackable_webhook::{Options, WebhookServer};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new().route("/", post(convert));
 
-    let options = Options::builder().disable_redirect().build();
+    let options = Options::builder()
+        .disable_redirect()
+        .socket_addr(([0, 0, 0, 0], 8443))
+        .build();
     let server = WebhookServer::new(router, options);
     server.run().await
 }
 
-async fn convert(Json(request): Json<ConversionRequest>) -> Json<ConversionResponse> {
-    Json(request.into())
+async fn convert(Json(review): Json<ConversionReview>) -> Json<ConversionReview> {
+    println!("{}", serde_json::to_string(&review).unwrap());
+    let request = ConversionRequest::from_review(review).unwrap();
+    let objects = request.objects.clone();
+    let response: ConversionResponse = ConversionResponse::for_request(request).success(objects);
+    println!("{}", serde_json::to_string(&response).unwrap());
+    Json(response.into_review())
 }
