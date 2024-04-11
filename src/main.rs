f5defe62d31d770133c5b6c5f7ab85b60b12b530
Nick Larsen <nick.larsen@stackable.tech>
2024-04-11 19:15:43 +0200

    feat: init console tracing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ use std::error::Error;
 
 use axum::{routing::post, Json, Router};
 use kube::core::conversion::{ConversionRequest, ConversionResponse, ConversionReview};
+use stackable_telemetry::Tracing;
 use stackable_webhook::{Options, WebhookServer};
+use tracing::level_filters::LevelFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let _trace_guard = Tracing::builder()
+        .service_name("dummy-webhook")
+        .with_console_output(LevelFilter::TRACE)
+        .build()
+        .init()?;
+
     let router = Router::new().route("/", post(convert));
 
     let options = Options::builder().bind_address([0, 0, 0, 0], 8443).build();
