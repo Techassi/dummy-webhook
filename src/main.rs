e207cc89692f7275f4292f720fe201387196e7f0
Nick Larsen <nick.larsen@stackable.tech>
2024-04-15 12:26:13 +0200

    chore: add ping endpoing for simple local testing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::error::Error;
 
-use axum::{routing::post, Json, Router};
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
 use kube::core::conversion::{ConversionRequest, ConversionResponse, ConversionReview};
 use stackable_telemetry::Tracing;
 use stackable_webhook::{Options, WebhookServer};
 use tracing::level_filters::LevelFilter;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Ping {
+    pub ping: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Pong {
+    pub pong: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -14,7 +30,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .build()
         .init()?;
 
-    let router = Router::new().route("/", post(convert));
+    let router = Router::new()
+        .route("/", post(convert))
+        .route("/", get(index))
+        .route("/ping", post(ping));
 
     let options = Options::builder().bind_address([0, 0, 0, 0], 8443).build();
     let server = WebhookServer::new(router, options);
@@ -29,4 +48,15 @@ async fn convert(Json(review): Json<ConversionReview>) -> Json<ConversionReview>
     let response: ConversionResponse = ConversionResponse::for_request(request).success(objects);
     println!("{}", serde_json::to_string(&response).unwrap());
     Json(response.into_review())
+}
+
+#[tracing::instrument(target = "ping")]
+async fn ping(Json(ping): Json<Ping>) -> Json<Pong> {
+    let pong = Pong { pong: ping.ping };
+    tracing::info!("ping-pong");
+    Json(pong)
+}
+
+async fn index() -> impl IntoResponse {
+    "hello"
 }
